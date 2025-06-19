9dc173f3e5f101b2b18dd47c142043f5befa4eec
HElnur <huseynove174@gmail.com>
2025-06-19 16:45:41 +0400

    Add application and infrastructure

--- TaskManagement.API/Program.cs
+++ TaskManagement.API/Program.cs
@@ -1,8 +1,17 @@
+using TaskManagement.Application;
+using TaskManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
